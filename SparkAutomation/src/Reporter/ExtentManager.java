872cceea7ab542141e872094ea57352e2b81a2a0
Dipesh Jain <dipesh.j@360logica.com>
2019-09-13 14:42:50 +0100

    Added timeStamp in extentreport file.

--- SparkAutomation/src/Reporter/ExtentManager.java
+++ SparkAutomation/src/Reporter/ExtentManager.java
@@ -1,16 +1,19 @@
 package Reporter;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.relevantcodes.extentreports.ExtentReports;
 
 public class ExtentManager {
-	
+	public static String timeStmp = new SimpleDateFormat("yyyyMMddHHmm'.txt'").format(new Date());
 	 private static ExtentReports extent;
 
 	  public synchronized static ExtentReports getReporter(){
 	      if(extent == null){
 	          //Set HTML reporting file location
 	          String workingDir = System.getProperty("user.dir");
-	          extent = new ExtentReports(workingDir+"\\ExtentReports\\ExtentReportResults.html", true);
+	          extent = new ExtentReports(workingDir+"\\ExtentReports\\ExtentReportResults"+timeStmp+".html", true);
 	      }
 	      return extent;
 	  }
