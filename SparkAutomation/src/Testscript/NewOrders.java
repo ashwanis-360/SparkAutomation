c2de32994d69b503581984ac3e45471c7c172ec2
Ayush.S <Ayush.S@360NDCPC155.360logica.com>
2019-11-12 21:08:03 +0530

    eol=methods added new order=xng script add

--- SparkAutomation/src/Testscript/NewOrders.java
+++ SparkAutomation/src/Testscript/NewOrders.java
@@ -13,7 +13,6 @@ public class NewOrders extends DriverTestcase {
 	public void EndtoEndOrderOnnet(Object[] Data) throws Exception {
 
 		Login.get().Login("Sieble");
-		// Login.get().VerifySuccessLogin("Sieble");
 		// newOrderOnnnet.get().Check(Data);
 		newOrderOnnnet.get().accountTabDetails(Data);
 		newOrderOnnnet.get().createCustomerOrder(Data);
@@ -82,7 +81,6 @@ public class NewOrders extends DriverTestcase {
 	public void Cease(Object[] Data) throws Exception {
 
 		Login.get().Login("Sieble");
-		// Login.get().VerifySuccessLogin("Sieble");
 		newOrderOnnnet.get().accountTabDetails(Data);
 		newOrderOnnnet.get().createCustomerOrder(Data);
 		newOrderOnnnet.get().productSelectionHelper(Data);
@@ -150,7 +148,6 @@ public class NewOrders extends DriverTestcase {
 	public void Cancel(Object[] Data) throws Exception {
 
 		Login.get().Login("Sieble");
-		// Login.get().VerifySuccessLogin("Sieble");
 		newOrderOnnnet.get().accountTabDetails(Data);
 		newOrderOnnnet.get().createCustomerOrder(Data);
 		newOrderOnnnet.get().productSelectionHelper(Data);
@@ -222,7 +219,6 @@ public class NewOrders extends DriverTestcase {
 	@Test(dataProviderClass = DataReader.class, dataProvider = "Mode")
 	public void Mod(Object[] Data) throws Throwable {
 		Login.get().Login("Sieble");
-		// Login.get().VerifySuccessLogin("Sieble");
 		newOrderOnnnet.get().accountTabDetails(Data);
 		newOrderOnnnet.get().createCustomerOrder(Data);
 		newOrderOnnnet.get().productSelectionHelper(Data);
@@ -326,7 +322,6 @@ public class NewOrders extends DriverTestcase {
 	public void OMPGenric(Object[] Data) throws Exception {
 
 		Login.get().Login("Sieble");
-		// Login.get().VerifySuccessLogin("Sieble");
 		// newOrderOnnnet.get().Check(Data);
 		newOrderOnnnet.get().accountTabDetails(Data);
 		newOrderOnnnet.get().createCustomerOrder(Data);
@@ -703,7 +698,73 @@ public class NewOrders extends DriverTestcase {
 			newOrderOnnnet.get().validateSlaMatrix(Data);
 		}
 	}
+ 
+	//As Add By Ayush
+		@Test(dataProviderClass = DataReader.class, dataProvider = "NewOrderOnnet")
+		public void XNGGeneric(Object[] Data) throws Exception {
+			
+			Login.get().Login("Sieble");
+			newOrderOnnnet.get().accountTabDetails(Data);
+			newOrderOnnnet.get().createCustomerOrder(Data);
+			newOrderOnnnet.get().productSelectionHelper(Data);
+			newOrderOnnnet.get().openServiceOrderNumber();
+			if (Data[8].toString().equalsIgnoreCase("Ethernet Hub")) {
+				newOrderOnnnet.get().addEthernetSiteHub(Data); // MethodAdded
+				newOrderOnnnet.get().hubSiteCustomize(Data); // MethodAdded
+				newOrderOnnnet.get().enterMandatoryFieldsInHeader(Data);// Updated
+				newOrderOnnnet.get().EnterDateInFooter(Data); // No change
+				newOrderOnnnet.get().EnterBillingDateInFooter(Data); // No change
+				newOrderOnnnet.get().EnterServiceChargeInFooter(Data, "2");
+				newOrderOnnnet.get().CommercialValidation(Data); // No Change
+				newOrderOnnnet.get().TechnicalValidation(Data); // Updated
+				newOrderOnnnet.get().DeliveryValidation(Data); // No Change
+				newOrderOnnnet.get().OrderCompleteEthernetHubSpoke(Data); // MethodAdded
+			} else if (Data[8].toString().equalsIgnoreCase("Ethernet Spoke")) {
+				newOrderOnnnet.get().addEthernetSiteSpoke(Data);// MethodAdded
+				newOrderOnnnet.get().spokeSiteCustomize(Data);// MethodAdded
+				newOrderOnnnet.get().enterMandatoryFieldsInHeader(Data);// Updated for spoke also
+				newOrderOnnnet.get().EnterDateInFooter(Data); // No change
+				newOrderOnnnet.get().EnterBillingDateInFooter(Data); // No change
+				newOrderOnnnet.get().EnterServiceChargeInFooter(Data, "2");
 
+				newOrderOnnnet.get().CommercialValidation(Data); // No Change
+				newOrderOnnnet.get().TechnicalValidation(Data); // Updated for spoke also
+				newOrderOnnnet.get().DeliveryValidation(Data); // No Change
+				newOrderOnnnet.get().OrderCompleteEthernetHubSpoke(Data);// updated same as hub
+			} else {
+				newOrderOnnnet.get().enterMandatoryFieldsInHeader(Data);
+				// add specifically for Wave and Ethernet Line and won't run for other products
+				newOrderOnnnet.get().addSiteADetails(Data);// added new
+				newOrderOnnnet.get().addSiteBDetails(Data);// added new
+				newOrderOnnnet.get().ASiteCustomize(Data);// added new
+				newOrderOnnnet.get().BSiteCustomize(Data);// added new
+				// End of methods spesific for Wave and Ethernet Line ------>
+				newOrderOnnnet.get().enterMandatoryDetailsInMiddleApplet(Data);
+				newOrderOnnnet.get().VoiceConfigTab(Data);
+				newOrderOnnnet.get().VoiceFeatureTab(Data);
+				newOrderOnnnet.get().NumberManagementTab(Data);
+				newOrderOnnnet.get().EnterDateInFooter(Data);
+				newOrderOnnnet.get().EnterBillingDateInFooter(Data);
+				newOrderOnnnet.get().EnterServiceChargeInFooter(Data, "2");
+
+				newOrderOnnnet.get().SelectAttachmentTab(Data);
+				newOrderOnnnet.get().UploadDocument(Data);
+				newOrderOnnnet.get().SelectServiceGroupTab(Data);
+				newOrderOnnnet.get().OperationAttribute(Data);
+				newOrderOnnnet.get().EnterInstallationChargeInFooter(Data);
+				newOrderOnnnet.get().MandatoryFields(Data);
+				newOrderOnnnet.get().CommercialValidation(Data);
+				newOrderOnnnet.get().TechnicalValidation(Data);
+				newOrderOnnnet.get().clickOnManualValidationB();
+				newOrderOnnnet.get().DeliveryValidation(Data);
+				newOrderOnnnet.get().clickOnManualValidationA();
+				newOrderOnnnet.get().CompletedValidation(Data);
+				//xng Code
+				newOrderOnnnet.get().LaunchingXNGApplication(Data);
+				
+			}
+
+			}
 
 
 
