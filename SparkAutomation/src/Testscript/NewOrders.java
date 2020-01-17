bc6b14ee4b5016b2c35e9b182a52406a5ed77ebe
abhay.s <abhay.s@360NDCPC098.360logica.com>
2020-01-17 20:22:29 +0530

    Updated CircuitReference for OffnetSLA

--- SparkAutomation/src/Testscript/NewOrders.java
+++ SparkAutomation/src/Testscript/NewOrders.java
@@ -650,27 +650,29 @@ public class NewOrders extends DriverTestcase {
 		newOrderOnnnet.get().productSelectionHelper(Data);
 		newOrderOnnnet.get().openServiceOrderNumber();
 		if (Data[8].toString().equalsIgnoreCase("Ethernet Hub")) {
-			newOrderOnnnet.get().addEthernetSiteHub(Data); // MethodAdded
-			newOrderOnnnet.get().hubSiteCustomize(Data); // MethodAdded
 			newOrderOnnnet.get().enterMandatoryFieldsInHeader(Data);// Updated
+			newOrderOnnnet.get().hubSiteCustomize(Data); // MethodAdded
+		
 			newOrderOnnnet.get().EnterDateInFooter(Data); // No change
 			newOrderOnnnet.get().EnterBillingDateInFooter(Data); // No change
 			newOrderOnnnet.get().EnterServiceChargeInFooter(Data, "2");
 			newOrderOnnnet.get().CommercialValidation(Data); // No Change
 			newOrderOnnnet.get().TechnicalValidation(Data); // Updated
+			newOrderOnnnet.get().CircuitReferenceGeneration(Data); 
 			newOrderOnnnet.get().DeliveryValidation(Data); // No Change
 			newOrderOnnnet.get().OrderCompleteEthernetHubSpoke(Data); // MethodAdded
 			newOrderOnnnet.get().validateSlaMatrix(Data);
 		} else if (Data[8].toString().equalsIgnoreCase("Ethernet Spoke")) {
-			newOrderOnnnet.get().addEthernetSiteSpoke(Data);// MethodAdded
-			newOrderOnnnet.get().spokeSiteCustomize(Data);// MethodAdded
 			newOrderOnnnet.get().enterMandatoryFieldsInHeader(Data);// Updated for spoke also
+			newOrderOnnnet.get().spokeSiteCustomize(Data);// MethodAdded
+			
 			newOrderOnnnet.get().EnterDateInFooter(Data); // No change
 			newOrderOnnnet.get().EnterBillingDateInFooter(Data); // No change
 			newOrderOnnnet.get().EnterServiceChargeInFooter(Data, "2");
 
 			newOrderOnnnet.get().CommercialValidation(Data); // No Change
 			newOrderOnnnet.get().TechnicalValidation(Data); // Updated for spoke also
+			newOrderOnnnet.get().CircuitReferenceGeneration(Data); 
 			newOrderOnnnet.get().DeliveryValidation(Data); // No Change
 			if (Data[74].toString().equals("Offnet")) {
 				newOrderOnnnet.get().CEOS_Offnet();
