a4f7a5b7a9d725d4ba08e42c6ae68ec552d05d90
ASharma20-adm <ASharma20-adm@ulvmctmttl103.INTERNAL.COLT.NET>
2020-02-27 08:55:12 +0000

    Devesh Kumar: Adding Data Reader for Novation

--- SparkAutomation/src/Driver/DataReader.java
+++ SparkAutomation/src/Driver/DataReader.java
@@ -88,6 +88,78 @@ public class DataReader {
 		 return listOfLists;
 		 
 	}
+	@DataProvider(name="NovationOrder", parallel=true)
+	public static Object[][] NovationOrder() throws IOException
+	{
+	
+		FileInputStream file = new FileInputStream(new File("src//Data//InputData.xlsx"));
+		//FileInputStream file = new FileInputStream(new File("C:\\Users\\ASharma19-adm\\git\\Spark\\SparkAutomation\\src\\Data\\EtherNetP2PNewOrder.xlsx"));
+		 XSSFWorkbook workbook = new XSSFWorkbook(file);
+		 XSSFSheet sheet = workbook.getSheet("Novation");
+		 XSSFRow r1=sheet.getRow(0);
+		 XSSFRow r;
+		 int rowindex=0;
+		 int totalrowrequired=0;
+		 for(int k=1;k<=sheet.getLastRowNum();k++){
+		 XSSFRow counter=sheet.getRow(k);
+		 if(counter.getCell(1).toString().equals("Yes"))
+		 {
+			 totalrowrequired=totalrowrequired+1;
+		
+		 }
+		 
+		 }
+		 Log.info("Total Data Set for Ethernet P2P will be"+totalrowrequired);
+		 Object[][] listOfLists = new Object[totalrowrequired][1];
+		//Object[][] data= new Object[sheet.getLastRowNum()][r.getLastCellNum()];  
+		 //Log.info("Total number of Columns" +r.getLastCellNum());
+		 //Log.info("Total number of Columns" +sheet.getLastRowNum());
+		 for(int i=2;i<=sheet.getLastRowNum();i++)
+		 { 
+			 r=sheet.getRow(i);
+			 Object[]  data=new Object[r1.getLastCellNum()];
+			 //Log.info(r.getLastCellNum());
+		if(r.getCell(1).toString().equals("Yes")){
+		   for(int j=3;j<=r1.getLastCellNum()-1;j++)
+			 {
+			   String strCellValue;
+			   try {
+				   if(r.getCell(j).getCellType()==Cell.CELL_TYPE_NUMERIC)
+				   {
+					   int value=(int)r.getCell(j).getNumericCellValue();
+					   strCellValue=String.valueOf(value);
+					   Log.info("The Value is in Int Format and Value is:"+strCellValue);
+					   
+				   }
+				   
+				   else {
+					   strCellValue=r.getCell(j).toString();
+					   Log.info("The Value of this cell is in String Format and Value is : "+strCellValue);
+				   }
+				   }
+				   catch(java.lang.NullPointerException e)
+				   {
+					   strCellValue="";
+					   Log.info("The Value of this cell is: "+strCellValue);
+				   }
+			   data[j-3] = strCellValue;
+			  
+			    //data[i-1][j] = ;
+			 
+			 }
+		   data[r1.getLastCellNum()-2]=r.getCell(0).toString();
+		   data[r1.getLastCellNum()-1]=r.getCell(1).toString();
+		   Log.info("The Value of this cell is: "+data[r1.getLastCellNum()-2]);
+		   Log.info("The Value of this cell is: "+data[r1.getLastCellNum()-1]);
+		   listOfLists[rowindex][0]=data;
+		   rowindex=rowindex+1;
+		 }
+		 //Log.info(data.toString());
+		 workbook.close();
+		 }
+		 return listOfLists;
+		 
+	}
 	@DataProvider(name="Abandoned", parallel=true)
 	public static Object[][] Abandoned() throws IOException
 	{
